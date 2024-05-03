491e0c5dd60d600bf2f645cd3c0d725d2b43c743
abas-linux <abasghlipour@gmail.com>
2024-05-04 00:40:49 +0330

    edit index pag

--- pasargad/settings.py
+++ pasargad/settings.py
@@ -95,16 +95,16 @@ WSGI_APPLICATION = 'pasargad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'root',
-#         'PASSWORD': 'SogASRGpoFOAlYBy78GysBdy',
-#         'HOST': 'pasargad-pc-db',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'SogASRGpoFOAlYBy78GysBdy',
+        'HOST': 'pasargad-pc-db',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
