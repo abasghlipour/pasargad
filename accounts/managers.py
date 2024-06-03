f747cbdc5f269c0160e03bca87ddd950e1e3a270
abas-linux <abasghlipour@gmail.com>
2024-06-03 15:11:00 +0330

    create user

---
+++ accounts/managers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,phone_number,email, name,family_name,password):
+        if not phone_number:
+            raise ValueError('Phone number must be set')
+        if not email:
+            raise ValueError('Email must be set')
+        if not name:
+            raise ValueError('Name must be set')
+        if not family_name:
+            raise ValueError('Family name must be set')
+        user=self.model(phone_number=phone_number,email=self.normalize_email(email),name=name,family_name=family_name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self,phone_number,email,password):
+        user= self.create_user(phone_number,email,password)
+        user.is_admin = True
+        user.is_staff=True
+        user.save(using=self.db)
+        return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name="شماره تلفن")
+    email = models.EmailField(unique=True, null=True, blank=True, verbose_name="ایمیل")
+    name = models.CharField(max_length=300, blank=True, null=True, verbose_name="نام")
+    family_name = models.CharField(max_length=300, blank=True, null=True, verbose_name="نام خانوادگی")
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    state = models.CharField(max_length=300, blank=True, null=True, verbose_name="استان")
+    city = models.CharField(max_length=300, blank=True, null=True, verbose_name="شهر")
+    is_active = models.BooleanField(default=True, verbose_name="فعال / غیرفعال")
+    is_admin = models.BooleanField(default=False, verbose_name="ادمین / عادی")
+    is_staff = models.BooleanField(default=False, verbose_name="کارمند / عادی")
+    is_seller = models.BooleanField(default=False, verbose_name='فروشنده / عادی')
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email', 'name']
+
+    def __str__(self):
+        return self.phone_number
+
+    def get_full_name(self):
+        return f'{self.name} {self.family_name}'
+
+    def has_perms(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
