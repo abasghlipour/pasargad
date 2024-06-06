adfcdd4ee837c61f6df773bf1a363d823366ccd6
abas_ku <abasghlipour@gmail.com>
2024-06-06 22:27:24 +0330

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
 
 class UserAdmin(BaseUserAdmin):
