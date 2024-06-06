3f1bbd1d3853c4d996d580928a29dd8910fe09d5
abas_ku <abasghlipour@gmail.com>
2024-06-06 22:13:40 +0330

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ['phone_number', 'is_admin', 'is_staff', 'is_active']
+    list_filter = ['is_admin', 'is_staff', 'is_active']
+    fieldsets = [
+        (None, {'filed': ('phone_number', 'email', 'name', 'family_name', 'password', 'avatar')}),
+        ('permissions', {'fields': ('is_active', 'is_admin', 'last_login',)})
+
+    ]
+    add_fieldsets = (
+        (None, {'fields': (
+            'phone_number', 'email', 'name', 'family_name', 'password1', 'password2',)}),
+    )
+    search_fields = ['phone_number']
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
